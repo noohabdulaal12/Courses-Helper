e73d46b3977371e930889fb68e778f4cbb284759
noohabdulaal12 <noohabdulaal12@gmail.com>
2026-06-03 00:43:59 +0300

    added more pages
    /Classrooms and /ClassroomTypes and requires logging in as coordinator or admin

---
+++ CoursesHelperMVC/Controllers/ClassroomTypesController.cs
@@ -0,0 +1,159 @@
+﻿using CoursesHelperWebAPI.Data;
+using CoursesHelperWebAPI.Models.App;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesHelperMVC.Controllers
+{
+    [Authorize(Roles = "Admin, Coordinator")]
+    public class ClassroomTypesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClassroomTypesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ClassroomTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ClassroomsTypes.ToListAsync());
+        }
+
+        // GET: ClassroomTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroomType = await _context.ClassroomsTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (classroomType == null)
+            {
+                return NotFound();
+            }
+
+            return View(classroomType);
+        }
+
+        // GET: ClassroomTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClassroomTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Capacity,Description")] ClassroomType classroomType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(classroomType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(classroomType);
+        }
+
+        // GET: ClassroomTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroomType = await _context.ClassroomsTypes.FindAsync(id);
+            if (classroomType == null)
+            {
+                return NotFound();
+            }
+            return View(classroomType);
+        }
+
+        // POST: ClassroomTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Capacity,Description")] ClassroomType classroomType)
+        {
+            if (id != classroomType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(classroomType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassroomTypeExists(classroomType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(classroomType);
+        }
+
+        // GET: ClassroomTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroomType = await _context.ClassroomsTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (classroomType == null)
+            {
+                return NotFound();
+            }
+
+            return View(classroomType);
+        }
+
+        // POST: ClassroomTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var classroomType = await _context.ClassroomsTypes.FindAsync(id);
+            if (classroomType != null)
+            {
+                _context.ClassroomsTypes.Remove(classroomType);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassroomTypeExists(int id)
+        {
+            return _context.ClassroomsTypes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CoursesHelperMVC/Controllers/ClassroomsController.cs
@@ -0,0 +1,166 @@
+﻿using CoursesHelperWebAPI.Data;
+using CoursesHelperWebAPI.Models.App;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesHelperMVC.Controllers
+{
+    [Authorize(Roles = "Admin, Coordinator")]
+    public class ClassroomsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClassroomsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Classrooms
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Classrooms.Include(c => c.ClassroomType);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Classrooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroom = await _context.Classrooms
+                .Include(c => c.ClassroomType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Create
+        public IActionResult Create()
+        {
+            ViewData["TypeId"] = new SelectList(_context.ClassroomsTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Classrooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TypeId,Description")] Classroom classroom)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(classroom);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.ClassroomsTypes, "Id", "Name", classroom.TypeId);
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroom = await _context.Classrooms.FindAsync(id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+            ViewData["TypeId"] = new SelectList(_context.ClassroomsTypes, "Id", "Name", classroom.TypeId);
+            return View(classroom);
+        }
+
+        // POST: Classrooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TypeId,Description")] Classroom classroom)
+        {
+            if (id != classroom.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(classroom);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassroomExists(classroom.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.ClassroomsTypes, "Id", "Name", classroom.TypeId);
+            return View(classroom);
+        }
+
+        // GET: Classrooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classroom = await _context.Classrooms
+                .Include(c => c.ClassroomType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+
+            return View(classroom);
+        }
+
+        // POST: Classrooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var classroom = await _context.Classrooms.FindAsync(id);
+            if (classroom != null)
+            {
+                _context.Classrooms.Remove(classroom);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassroomExists(int id)
+        {
+            return _context.Classrooms.Any(e => e.Id == id);
+        }
+    }
+}
